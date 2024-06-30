68bbfe4bce129dda64be86b172966c8b32ec450d
theonlysroy <tirtha13july2002sonofmp@gmail.com>
2024-07-01 05:03:18 +0530

    student routes added

--- src/routes/student.route.js
+++ src/routes/student.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { register_student } from "../controllers/student.controller.js";
+import {
+  register_student,
+  login_student,
+} from "../controllers/student.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
-// const upload = multer({ dest: "uploads/" });
-
 // post routes
 router.route("/register").post(upload.single("avatar"), register_student);
+router.route("/login").post(upload.single("avatar"), login_student);
 
 export default router;
